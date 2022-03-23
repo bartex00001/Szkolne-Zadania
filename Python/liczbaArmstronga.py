2a4074a04720d21b878c67fbd0e34ee1215e05b1
Bartłomiej Stefański <30830383+bartex00001@users.noreply.github.com>
2022-03-23 18:24:42 +0000

    Naprawiona indentacja komentarzy?

--- Python/liczbaArmstronga.py
+++ Python/liczbaArmstronga.py
@@ -1,13 +1,13 @@
 # Sprawdz czy podana liczba jest 'armstrongowa'
 
-strNum = input("Wprowadź liczbę: ")   # wczytujemy od uzytkownika liczbe JAKO TEKST
-Num = int(strNum)					  # przypisujemy zmiennej Num wczytana liczbe jako int
+strNum = input("Wprowadź liczbę: ")		# wczytujemy od uzytkownika liczbe JAKO TEKST
+Num = int(strNum)						# przypisujemy zmiennej Num wczytana liczbe jako int
 
-dlugosc = len(strNum)				  # zapamietaj dlugosc (przyda sie pozniej)
+dlugosc = len(strNum)					# zapamietaj dlugosc (przyda sie pozniej)
 suma = 0
 
-for i in strNum:                      # iterujemy po literach w tekscie
-    suma += int(i) ** dlugosc         # dodajemy do sumy cyfre podniesiona do potegi dlugosci calej wprowadzonej liczby
+for i in strNum:						# iterujemy po literach w tekscie
+    suma += int(i) ** dlugosc			# dodajemy do sumy cyfre podniesiona do potegi dlugosci calej wprowadzonej liczby
 
 if suma == Num:
 	print("OK")
