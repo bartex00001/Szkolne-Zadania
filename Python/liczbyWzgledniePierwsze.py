95a57cdbec876bb11ebe2b49a296c7f936d14e6f
Bartek Stefaski <bartex00001@gmail.com>
2022-03-26 17:27:49 +0100

    Liczby względnie pierwsze

--- Python/liczbyWzgledniePierwsze.py
+++ Python/liczbyWzgledniePierwsze.py
@@ -1,2 +1,21 @@
 # Program sprawdzi czy liczby sa wzglednie piersze
 
+def czyLiczbyWzgledniePierwsze(a, b):
+    minimum = min(a, b)
+
+    # Inna definicja liczb Względnie pierwszych:
+    # Dwie liczby, które nie mają wspólnych dzielników
+    for i in range(2, minimum):
+        # Jeżeli jest wspólny dzielnik, to nie są względnie pierwsze
+        if a % i == 0 and b % i == 0:
+            return False
+    return True
+
+# Wczytaj dwie liczby całkowite
+a = int(input("Podaj pierwszą liczbę: "))
+b = int(input("Podaj drugą liczbę: "))
+
+if czyLiczbyWzgledniePierwsze(a, b):
+    print("Tak")
+else:
+    print("Nie")
