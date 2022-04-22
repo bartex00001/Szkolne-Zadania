01c798e0c25cddc919f41ff28075629a5e783c08
Wojciech Orański <63315952+PWok@users.noreply.github.com>
2022-04-22 10:04:20 +0200

    Add files via upload

---
+++ Python/schematHornera-normalnie.py
@@ -0,0 +1,22 @@
+# wprowadzanie danych
+
+stopien = int(input("Podaj stopień wielomianu: "))
+
+podstawy = []
+
+for i in range(stopien + 1):
+    podstawy.append(int(input("Podaj podstawę przy " + stopień-i + " potędze: ")))
+
+x = int(input("Podaj niewiadomą x: "))
+
+# obliczanie wartosci wielomianu
+
+
+value = podstawy[0]
+
+for i in range(stopien):
+
+    value = value*x+podstawy[i+1]
+
+print(value)
+
