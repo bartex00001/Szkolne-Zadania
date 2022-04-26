ba0f4ffa352d4e35f2e6b984933fafd6acf95244
Bartlomiej Sefanski <bartex00001@gmail.com>
2022-04-26 17:39:30 +0200

    Implementacja zliczania liter

---
+++ Python/policzLitery.py
@@ -0,0 +1,13 @@
+# W podanym tekście program policzy wystąpienia wskazanej litery
+
+# Wczytaj tekst i literę
+tekst = input("Podaj tekst: ")
+litera = input("Podaj literę: ")
+
+# Zlicz wystąpienia
+licznik = 0
+for znak in tekst:
+    if znak == litera:
+        licznik += 1
+
+print("Liczba wystapien litery w tekscie:", licznik)
