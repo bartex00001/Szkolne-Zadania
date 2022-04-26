9bfa033b95600a3a6d616ed74d3486ffb1a3b636
Bartlomiej Sefanski <bartex00001@gmail.com>
2022-04-26 21:01:10 +0200

    Implementacja konwersji systemów

---
+++ Python/dwojkowyNaDziesietny.py
@@ -0,0 +1,19 @@
+# Program zamieni zapis w systmemie dwojkowym na dziesiętny
+
+# Wczytaj binarną liczbę jako tekst
+liczbaBinarna = input("Podaj liczbę dwojkową: ")
+
+wynik = 0
+
+# zaczynamy od najbardziej znaczącego bitu -> start = 2^(len-1)
+potega = 2 ** (len(liczbaBinarna)-1)
+
+for cyfra in liczbaBinarna:
+    # Dodajemy tylko przy jedynce
+    if cyfra == "1":
+        wynik += potega
+    
+    # Idziemy do mniej znaczących bitów
+    potega = potega // 2
+
+print("Wynik:", wynik)
