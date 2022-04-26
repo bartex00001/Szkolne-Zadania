ca639e078a285890236fb75d8963d6c6e7b42eeb
Bartlomiej Sefanski <bartex00001@gmail.com>
2022-04-26 17:32:08 +0200

    Funkcja do palindromów

--- Python/palindrom.py
+++ Python/palindrom.py
@@ -8,3 +8,14 @@ if wyraz == wyraz[::-1]:
     print("Wyraz jest palindromem")
 else:
     print("Wyraz nie jest palindromem")
+
+
+# Alternatywnie - sprawdzanie w funkcji
+def czyPalindrom(word):
+    # Tutaj zwracamyh True/False, albo wypisujemy komunikat
+    return word == word[::-1]
+
+if czyPalindrom(wyraz):
+    print("Wyraz jest palindromem - z funkcji")
+else:
+    print("Wyraz nie jest palindromem - z funkcji")
