111343b6d57a15b708da9668463f359e6268dc0f
Wojciech Orański <63315952+PWok@users.noreply.github.com>
2022-04-29 09:50:41 +0200

    Add files via upload

---
+++ Python/liczbyDoskonale.py
@@ -0,0 +1,16 @@
+# uzyskanie wszystkich dzielników liczby
+def dzielniki(x):
+    dziel = []
+
+    for i in range(1, x):   #jest x bo chcemy tylko dzielnik właściwe tj. mniejszse od x
+        if x%i == 0:
+            dziel.append(i)
+
+    return dziel
+
+
+liczba = int(input("Podaj liczbę do sprawdzenia: "))
+if liczba == sum(dzielniki(liczba)):
+    print("Liczba doskonała")
+else:
+    print("Liczba NIE doskonała")
