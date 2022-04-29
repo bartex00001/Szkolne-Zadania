f9db6877d867d8b48c2844d068ff081131c9c80e
Bartłomiej Stefański <30830383+bartex00001@users.noreply.github.com>
2022-04-29 07:31:37 +0000

    Dodanie algorytmu nww i zmiana nazwy

--- Python/euklides-NWD-NWW.py
+++ Python/euklides-NWD-NWW.py
@@ -7,8 +7,13 @@ def nwd(a, b):
     else:
         return nwd(b, a % b)
 
+# Implementacja najmniejszej wspólnej wielokrotności
+def nww(a, b):
+	return (a*b) // nwd(a, b)
+
 # Wczytaj dwie liczby n1 i n2
 n1 = int(input("Podaj pierwszą liczbę: "))
 n2 = int(input("Podaj drugą liczbę: "))
 
 print("NWD podanych liczb:", nwd(n1, n2))
+print("NWW podanych liczb:", nww(n1, n2))
